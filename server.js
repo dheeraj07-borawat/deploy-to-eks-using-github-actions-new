9e37834720f732f018acd62c8949dbcd7cb4c78a
dheeraj07-borawat <dheerajborawat19@gmail.com>
2023-06-16 01:14:48 +0530

    actions

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from from Dhiraj-Borawat007, this was awesome-Devops Journey';
+  const msg = 'Hello from from Dhiraj-Borawat, this was awesome-Devops Journey';
   res.send(getPage(msg));
 });
 
